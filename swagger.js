af93eda2b60fbd607edd8a67a1c71a88aeaf1b0a
Thomas Garcia <tjg9265@yahoo.com>
2026-04-04 20:27:39 -0600

    Added review routes updates and minor fixes

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: "Healthy Meals Api",
         description: "Api for managing healthy meals"
     },
-    host: "https://healthy-meals-api.onrender.com",
+    host: "healthy-meals-api.onrender.com",
     schemes: ["https","http"]
 };
 
