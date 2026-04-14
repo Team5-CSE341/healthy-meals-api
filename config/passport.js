3ad1cff1dd950163a29e0adc6bd7312f2a3257c4
kpcastillo <krn_iveth09@hotmail.com>
2026-04-13 19:32:08 -0700

    Changed function to initialize database on passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,36 +1,52 @@
 const passport = require("passport")
 const GitHubStrategy = require("passport-github2").Strategy
-const connectDB = require("../db/connection")
+const {getDb} = require("../db/connection")
 
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.GITHUB_CALLBACK_URL
-},
-async (accessToken, refreshToken, profile, done) => {
-  try {
-    const db = await connectDB()
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL
+    },
 
-    let user = await db.collection("users").findOne({
-      githubId: profile.id
-    })
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const db = getDb()
 
-    if (!user) {
-      user = {
-        githubId: profile.id,
-        username: profile.username,
-        displayName: profile.displayName
+        let user = await db.collection("users").findOne({
+          githubId: profile.id
+        })
+
+        if (!user) {
+          const result = await db.collection("users").insertOne({
+            githubId: profile.id,
+            username: profile.username,
+            displayName: profile.displayName
+          });
+
+          user = await db.collection("users").findOne({
+            _id: result.insertedId
+          });
+        }
+
+        return done(null, user._id.toString())
+
+      } catch (err) {
+        return done(err, null)
       }
-
-      await db.collection("users").insertOne(user)
     }
+  )
+);
 
-    return done(null, user)
-
+passport.serializeUser((userId, done) => done(null, userId))
+passport.deserializeUser(async (userId, done) => {
+  try {
+    const { ObjectId } = require("mongodb");
+    const db = getDb();
+    const user = await db.collection("users").findOne({ _id: new ObjectId(userId) });
+    done(null, user);
   } catch (err) {
-    return done(err, null)
+    done(err);
   }
-}))
-
-passport.serializeUser((user, done) => done(null, user))
-passport.deserializeUser((user, done) => done(null, user))
+});
